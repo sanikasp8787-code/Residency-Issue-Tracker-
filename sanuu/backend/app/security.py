1cd471a6f68992bb5605fcd13e46e7e58415b1f3
sanikasp8787-code <sanikasp8787@gmail.com>
2026-08-08 20:55:39 +0530

    Refactor password hashing functions

--- sanuu/backend/app/security.py
+++ sanuu/backend/app/security.py
@@ -6,9 +6,6 @@ pwd_context = CryptContext(
 )
 
 def hash_password(password: str):
-    print("Password:", password)
-    print("Type:", type(password))
-    print("Length:", len(password))
     return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str):
